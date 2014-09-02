83535d3b9c9536e3ceabdd56953c501f784c4031
Charles A. Labanowski <clabanow@gmail.com>
2014-09-02 20:15:54 +0200

    Adds new tests for about and help pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,27 +1,43 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
-  describe "GET 'home'" do
+  describe "Home page" do
+
     it "returns http success" do
       get 'home'
       response.should be_success
     end
-  end
 
-  describe "GET 'contact'" do
-    it "returns http success" do
-      get 'contact'
-      response.should be_success
+    it "should have the content 'Sample App'" do
+      visit '/pages/home'
+      expect(page).to have_content('Sample App')
     end
+
   end
 
-  describe "GET 'about'" do
+  describe "Help page" do
+    
+    it "should have the content 'Help'" do
+      visit '/pages/help'
+      expect(page).to have_content('Help')
+    end
+
+  end
+
+  describe "About page" do
+
     it "returns http success" do
       get 'about'
       response.should be_success
     end
-  end
 
+    it "should have the content 'About Us'" do
+      visit '/pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+  end
 
 end
